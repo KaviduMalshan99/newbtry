6a630fa4844957bb8a511cfe35aa1a750f6b6032
Manulakavishka <86448764+Manulakavishka@users.noreply.github.com>
2024-12-31 14:48:26 +0530

    company details can be access anywhere

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Models\Company;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +24,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        view()->composer('*', function ($view) {
+            $companyDetails = Company::first();
+            $view->with('companyDetails', $companyDetails);
+        });
     }
 }
