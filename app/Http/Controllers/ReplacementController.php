01733298b5fcffc3122353831c1e46cdb6fe6f1a
Manulakavishka <86448764+Manulakavishka@users.noreply.github.com>
2025-01-28 15:20:20 +0530

    pos and replacement updates

--- app/Http/Controllers/ReplacementController.php
+++ app/Http/Controllers/ReplacementController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Models\Battery;
+use App\Models\BatteryModelNumber;
 use App\Models\BatteryOrder;
 use App\Models\Company;
 use App\Models\OldBattery;
@@ -267,6 +268,19 @@ class ReplacementController extends Controller
 
             // Fetch the order
             $batteryOrder = BatteryOrder::findOrFail($validatedData['order_id']);
+
+            // Update the is_active column in battery_model_numbers table
+            foreach ($items as $item) {
+                if (isset($item['battery_id'])) {
+                    BatteryModelNumber::where('battery_id', $item['battery_id'])
+                        ->where('is_active', 1)
+                        ->limit($item['quantity'])
+                        ->update([
+                            'is_active' => 0, // Set is_active to 0
+                            'battery_order_id' => $batteryOrder->id, // Set battery_order_id to the new order's ID
+                        ]);
+                }
+            }
 
             // Get items to remove from payload
             $itemsToRemove = json_decode($validatedData['customer_order_items'], true);
